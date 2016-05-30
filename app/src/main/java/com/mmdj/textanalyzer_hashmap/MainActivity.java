3b1f0ccdf216ff6788dcba904e7fe649964bd2d5
mmdj <mdolzhansky@gmail.com>
2016-05-30 18:05:49 +0300

    + TODO

--- app/src/main/java/com/mmdj/textanalyzer_hashmap/MainActivity.java
+++ app/src/main/java/com/mmdj/textanalyzer_hashmap/MainActivity.java
@@ -1,11 +1,13 @@
 package com.mmdj.textanalyzer_hashmap;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,7 +18,8 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText EditTextInput;
+    ListView LstVw_Result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +30,32 @@ public class MainActivity extends AppCompatActivity {
      * Main function
      ***************************************************/
     public void analyzeText(View view) {
-        String[] strArray;
+        String[] strArray = new String[0];
 
-        EditText EditTextInput;
+        //doing change to weight of ListView
+        LstVw_Result = (ListView) findViewById(R.id.lstVw_result);
+        LstVw_Result.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 0, 1f));
+
+
+
         EditTextInput = (EditText) findViewById(R.id.edtTxt_input);
-
 
         String TextInString = null;
         if (EditTextInput != null) {
             TextInString = EditTextInput.getText().toString();
         }
 
-        if (TextInString.isEmpty()) {                            //checking text
+        if (TextInString != null && TextInString.isEmpty()) {                            //checking text
             doToast("There no text for analyze.");
             return;
         }
 
-        strArray = TextInString.split(getString(R.string.spaceForDividingWords));
+         /*** instance 2 to remove punctuation.***/
+        if (TextInString != null) {
+            Intent intent = new Intent(this, Result_Activity.class);
+            startActivity(intent);
+            strArray = TextInString.split("[\\p{Punct}\\s]+");
+        }
 
 
         List<Map.Entry<String, Integer>> result = analyzeArray(strArray);
@@ -92,13 +104,23 @@ public class MainActivity extends AppCompatActivity {
         for (String string : strArray) {
             int value = 1;
 
-            if (map.containsKey(string) ) {//TODO without points
+            /* instance 2 to remove punctuation.
+            if (string.endsWith(".") ||
+                    string.endsWith(",") ||
+                    string.endsWith("!") ||
+                    string.endsWith("?") ||
+                    string.endsWith("-")) {
+                string = string.substring(0, string.length() - 1);
+            }
+            */
+            if (map.containsKey(string)) {
                 value = map.get(string);
                 value++;
             }
 
             map.put(string, value);
         }
+
     }
 
 
@@ -108,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
     private void doToast(String message) {
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
-
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
     }
